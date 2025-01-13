657309a8b2cae679db94c95f7aa6b7533034c8e6
mav-rik <mav777art@gmail.com>
2025-01-13 21:27:19 +0100

    feat: improve rolldown/vite plugins

--- src/plugins/rolldown.ts
+++ src/plugins/rolldown.ts
@@ -6,11 +6,18 @@ import type { Plugin } from 'rolldown'
 
 import { createDyeReplacements } from './common'
 
+/**
+ * Rolldown plugin for dye
+ *
+ * !IMPORTANT: add
+ * "extends": "./node_modules/@prostojs/dye/tsconfig.dye.json",
+ * to your tsconfig.json
+ */
 export default function rolldownPlugin(): Plugin {
   return {
     name: '@prostojs/dye',
     options(o) {
-      o.define = { ...createDyeReplacements(true), ...o.define }
+      o.define = { ...createDyeReplacements(), ...o.define }
       return o
     },
   }
--- src/plugins/vite.ts
+++ src/plugins/vite.ts
@@ -6,6 +6,13 @@ import type { Plugin } from 'vite'
 
 import { createDyeReplacements } from './common'
 
+/**
+ * Vite plugin for dye
+ *
+ * !IMPORTANT: add
+ * "extends": "./node_modules/@prostojs/dye/tsconfig.dye.json",
+ * to your tsconfig.json
+ */
 export default function vitePlugin(): Plugin {
   return {
     name: '@prostojs/dye',
